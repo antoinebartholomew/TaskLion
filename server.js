cb4246f2f3ebf725a46c89a7409f21b17ad90f7a
dbailey25 <dtbailey25@gmail.com>
2018-03-20 12:49:39 -0400

    deploy

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ var db = require("./models");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set("views", __dirname + "views");
+app.set("views", __dirname + "/views");
 // app.set("views", "views");
 app.set("view engine", "jade");
 // app.set("view engine","ejs")
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 // app.use(express.static("public"));
 app.use(express.static("nodules/bootstrap/dist"));
 
